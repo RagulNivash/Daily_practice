3a35780c5426a2bb41a055e383eee3bcc7b07ac7
RagulNivash <49038494+RagulNivash@users.noreply.github.com>
2024-09-04 10:45:20 -0400

    Update design_code.sv

--- Day_13_DFF_UVM/design_code.sv
+++ Day_13_DFF_UVM/design_code.sv
@@ -1,1 +1,24 @@
+module dff(input rst, clk,din, output dout);
+
+  always @(posedge clk) begin
+    if(rst==1'b1)
+      dout<=1'b0;
+    else dout<= din;      
+  end
+endmodule
+
+
+///////////////////////////////////////////
+
+interface fif;
+
+logic rst;
+  logic din;
+  logic dout;
+  logic clk;
+
+
+endinterface
+
+
 
