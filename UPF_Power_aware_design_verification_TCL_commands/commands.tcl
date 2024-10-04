8374a451a5a56443abfbacc69016029d6e0725ec
RagulNivash <49038494+RagulNivash@users.noreply.github.com>
2024-10-04 08:07:36 -0400

    Update commands.tcl

--- UPF_Power_aware_design_verification_TCL_commands/commands.tcl
+++ UPF_Power_aware_design_verification_TCL_commands/commands.tcl
@@ -1,1 +1,17 @@
+set_scope dut
+
+create_power_domain PD_dut\
+-include_scope
+
+create_power_domain PD_cpu\
+-elements {cpu}
+
+create_power_domain PD_video
+-elements{video}
+
+create_power_domain PD_cpuB
+-elements {cpu/B1 cpu/B2}
+
+create_power_domain PD_videoHash/
+-elements {video/Bh2 video Bh3}
 
