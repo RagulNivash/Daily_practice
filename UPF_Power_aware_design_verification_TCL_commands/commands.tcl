e905cd81af005cf368c0d0f64ba932df97348437
RagulNivash <49038494+RagulNivash@users.noreply.github.com>
2024-10-04 08:32:05 -0400

    Update commands.tcl

--- UPF_Power_aware_design_verification_TCL_commands/commands.tcl
+++ UPF_Power_aware_design_verification_TCL_commands/commands.tcl
@@ -14,4 +14,27 @@ create_power_domain PD_cpuB
 
 create_power_domain PD_videoHash/
 -elements {video/Bh2 video Bh3}
+//////////////////////////////////////////////////////////////////////////////
+create_supply_net VDD_H -domain PD_cpu
+create_supply_net VDD_H -domaiin PD_cupB -reuse
+create_supply_net VDD_L -doamin PD_video
 
+create_supply_port VDD_H -domain PD_cpu\
+-direction <in/out/inout>
+
+connect_supply_net VDD_H -ports VDD_H
+
+create_supply_port VDD_H -ports PD-cpu
+create_supply_port VSS -ports PD_cpu
+create_supply_port VSS -doamin PD_video
+
+create_supply_port Vint -domain PD_cpu -direction output
+create_supply_port VInt -domain PD_video
+
+create_supply_net VDD_H -domaain PD_cpu
+create_supply_net Vint -domain PD_cpu
+create_supply_net Vint -domain PD_video -reuse
+
+connect_supply_net VDD_H -ports VDD_H
+connect-supply_net Vint -porst Vint
+/////////////////////////////////////////////////////////////////////////////////////
