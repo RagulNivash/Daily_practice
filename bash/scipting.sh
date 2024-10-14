3ea805ff173c2c72d6fa8e5471f02bdf0b2a89e4
RagulNivash <49038494+RagulNivash@users.noreply.github.com>
2024-10-14 15:01:21 -0400

    Update scipting.sh

--- bash/scipting.sh
+++ bash/scipting.sh
@@ -16,5 +16,74 @@
 
 # else
 #  echo"no name"
-fi
+# fi
+ 
+ifconfig |grep broadcast | awk '{print $2}'
+
+alias ipadress="ifcongif ! grep broadcast | awk '{print $2}'"
+
+alias ipadress="echo$(ifcongif ! grep broadcast | awk '{print $2}')"
+
+
+##################################
+
+if [${1,,}= herbert]; then
+ echo "Oh, you"
+elif [${1,,,}=help]; then
+ echo "Just"
+ else echo " I do"
+ fi
+ ##################################
+
+case ${1,,,} in 
+ herbert | administrator)
+   echo " you"
+   ;;
+ help)
+  echo "Jusyt"
+ ;;
+ *)
+   echo " I do"
+esac
+
+###########################################
+
+showuptime(){
+   up=$(uptime -p | cut -c4-)
+   since=$(uptime -s)
+   cat << EOF
+
+   ------------------------------------------
+THis
+-----------------------------------
+EOF
+}
+
+  
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
  
