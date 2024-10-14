1d517e1534459582498736a727abecde366c8af0
RagulNivash <49038494+RagulNivash@users.noreply.github.com>
2024-10-14 12:17:25 -0400

    Create scipting.sh

---
+++ bash/scipting.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# STRING "mass"
+# echo "Ragul is $STRING"
+
+
+#echo "I am gethu $1 "
+
+#echo $(whoami)
+
+# echo "what is your name"
+# read name
+
+# if [$name];then
+#   echo "gethu"
+
+# else
+#  echo"no name"
+fi
+ 
